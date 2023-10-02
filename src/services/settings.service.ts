03d8c8fc524775b44129efc220bca11858548733
alexei <alexeialvarez4@gmail.com>
2023-10-01 20:34:25 -0600

    fix: route to get settings :bug: :'v'

--- src/services/settings.service.ts
+++ src/services/settings.service.ts
@@ -1,6 +1,6 @@
 import { type Settings } from '@/models/time.interface.ts'
 
 export const getAppSettings = async () => {
-  const response = await fetch('/public/data/settings.json')
+  const response = await fetch('/data/settings.json')
   return await (await response.json() as Promise<Settings[]>)
 }
